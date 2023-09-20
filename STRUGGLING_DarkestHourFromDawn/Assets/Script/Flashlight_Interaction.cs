8fa83cb44b04d41a8cdecfa8f1e87e09c7d6d056
YuYongjin <radio020112@gmail.com>
2023-09-20 20:28:57 +0900

    [Test] CollisionStay Tag 이동조작, [Test] 손전등 기능 추가
    이 모든 것은 Test 임을 밝힘.

---
+++ STRUGGLING_DarkestHourFromDawn/Assets/Script/Flashlight_Interaction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashlight_Interaction : MonoBehaviour
+{
+    public GameObject field_Flashlight;
+    public GameObject UI_Flashlight;
+    bool UI_FlashlightON;
+
+    public GameObject player_Flashlight;
+
+    public bool isOn;
+    bool isHasFlashlight = false;
+
+    void Start()
+    {
+        isOn = true;
+        UI_FlashlightON = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //¹Ù´Ú¿¡ ´êÀ¸¸é
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Flashlight"))
+        {
+            isHasFlashlight = true;
+            field_Flashlight.gameObject.SetActive(false);
+            UI_Flashlight.gameObject.SetActive(true);
+            UI_FlashlightON = true;
+            if (UI_FlashlightON == true)
+            {
+                Destroy(UI_Flashlight.gameObject, 3);
+            }
+
+            player_Flashlight.gameObject.SetActive(true);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && isHasFlashlight)
+        {
+            if (isOn)
+            {
+                player_Flashlight.gameObject.SetActive(false);
+                isOn = false;
+            }
+            else if (!isOn)
+            {
+                player_Flashlight.gameObject.SetActive(true);
+                isOn = true;
+            }
+        }
+    }
+
+}
---
+++ STRUGGLING_DarkestHourFromDawn/Assets/Script/PlayerMoveController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMoveController : MonoBehaviour
+{    
+    Rigidbody rd;
+    public float moveSpeed = 1f;
+    private bool isMoving;
+
+    public GameObject Flashlight;
+    //public GameObject[] weapons;
+    //public bool[] hasWeapons;
+
+    bool iDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        isMoving = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //바닥에 닿으면
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            //이동이 가능한 상태로 변경
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+    }
+
+    void GetInput()
+    {
+        iDown = Input.GetButtonDown("Interaction");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //interaction();
+        if (isMoving)
+        {
+            Vector3 HorizontalMove = Vector3.zero;
+            Vector3 VerticalMove = Vector3.zero;
+
+            Vector3 moveDistance = Vector3.zero;
+
+            HorizontalMove = transform.right * Input.GetAxis("Horizontal") * moveSpeed;
+            VerticalMove = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+
+            moveDistance = (HorizontalMove + VerticalMove);
+
+            rd.AddForce(moveDistance);
+        }
+    }
+    //void interaction()
+    //{
+    //    if (iDown && Flashlight != null)
+    //    {
+    //        if(this.gameObject.layer == LayerMask.NameToLayer("Flashlight"))
+    //        {
+                
+    //        }
+    //    }
+    //}
+}
