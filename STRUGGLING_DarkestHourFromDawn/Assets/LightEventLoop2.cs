6dae1690f4671e950f7f5bcc8b59fa23783cae72
blocks2094 <blocks2094@g.shingu.ac.kr>
2023-11-02 16:08:40 +0900

    fix: Light Event 코루틴 수정 add: 해당 이벤트 SurpriseEvent 추가

--- STRUGGLING_DarkestHourFromDawn/Assets/LightEventLoop2.cs
+++ STRUGGLING_DarkestHourFromDawn/Assets/LightEventLoop2.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class LightEventLoop2 : MonoBehaviour
 {
     public GameObject[] lightList;
-    public GameObject bigBear;
-    public GameObject pcCamera;
+    //public GameObject bigBear;
+    //public GameObject pcCamera;
 
-    public Transform bigBearT;
+    //public Transform bigBearT;
 
-    public PlayerV2 player;
+    //public PlayerV2 player;
     public Flashlight_Switch flashLight;
 
-    float realTime;
+    public bool SurpriseOn;
+    public bool test;
+
+    public float realTime;
 
     void Start()
     {
-
+        SurpriseOn = false;
+        test = false;
     }
 
     public void LightEvent()
     {
         StartCoroutine(LightEventCO());
+
     }
 
+    public void FlashlightEvent()
+    {
+        if (!test) return;
+        if (SurpriseOn) return;
+        
+        flashLight.eIDown = false;
+        flashLight.LightOff();        
+    }
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -36,46 +49,67 @@ public class LightEventLoop2 : MonoBehaviour
     // 특정 상황에서 업데이트할 때, 코루틴 사용
     IEnumerator LightEventCO()
     {
-        while (realTime < 7.5)
-        {
-            realTime += Time.deltaTime;
-            if (realTime >= 0.1f)
-            {
-                flashLight.eIDown = true;
-                flashLight.LightOff();
-                lightList[0].SetActive(false);
-                Debug.Log("실행");
-            }
-            if (realTime >= 0.6f)
-            {
-                lightList[1].SetActive(false);
-            }
-            if (realTime >= 1.1f)
-            {
-                lightList[2].SetActive(false);
-            }
-            if (realTime >= 1.6f)
-            {
-                lightList[3].SetActive(false);
-            }
-            if (realTime >= 2.1f)
-            {
-                lightList[4].SetActive(false);
-            }
-            if (realTime >= 6.5f)
-            {
-                // 이 부분은 그냥 특정 콜라이더를 밟을 때 실행 되도록 하는 것은 어떤가?
-                //player.moveSpeed = 0;
-                //bigBear.SetActive(true);
-                //pcCamera.transform.LookAt(bigBearT);
-                //lightList[0].SetActive(true);
-                //lightList[1].SetActive(true);
-                //lightList[2].SetActive(true);
-                //lightList[3].SetActive(true);
-                //lightList[4].SetActive(true);
-            }
-            yield return null;
-        }
+        yield return new WaitForSeconds(0.1f);
+        lightList[0].SetActive(false);
+        test = true;
+        Debug.Log("실행");
+        yield return new WaitForSeconds(0.6f);
+        lightList[1].SetActive(false);
+        yield return new WaitForSeconds(1.1f);
+        lightList[2].SetActive(false);
+        yield return new WaitForSeconds(1.6f);
+        lightList[3].SetActive(false);
+        lightList[4].SetActive(false);
+        yield return new WaitForSeconds(2.1f);
+        test = false;
+
+        //yield return null;
+
+        //while (realTime < 7.5)
+        //{
+        //    realTime += Time.deltaTime;
+        //    if (realTime >= 0.1f)
+        //    {
+        //        flashLight.eIDown = true;
+        //        flashLight.LightOff();
+        //        lightList[0].SetActive(false);
+        //        Debug.Log("실행");
+        //    }
+        //    if (realTime >= 0.6f)
+        //    {
+        //        lightList[1].SetActive(false);
+        //    }
+        //    if (realTime >= 1.1f)
+        //    {
+        //        lightList[2].SetActive(false);
+        //    }
+        //    if (realTime >= 1.6f)
+        //    {
+        //        lightList[3].SetActive(false);
+        //    }
+        //    if (realTime >= 2.1f)
+        //    {
+        //        lightList[4].SetActive(false);
+        //    }
+        //    if (realTime >= 6.5f)
+        //    {
+        // 이 부분은 그냥 특정 콜라이더를 밟을 때 실행 되도록 하는 것은 어떤가?
+        //player.moveSpeed = 0;
+        //bigBear.SetActive(true);
+        //pcCamera.transform.LookAt(bigBearT);
+        //lightList[0].SetActive(true);
+        //lightList[1].SetActive(true);
+        //lightList[2].SetActive(true);
+        //lightList[3].SetActive(true);
+        //lightList[4].SetActive(true);
+        //}
+        //yield return null;
+        //}
+    }
+
+    private void Update()
+    {
+        FlashlightEvent();
     }
 }
 
--- STRUGGLING_DarkestHourFromDawn/Assets/SurpriseEvent.cs
+++ STRUGGLING_DarkestHourFromDawn/Assets/SurpriseEvent.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class SurpriseEvent : MonoBehaviour
 {
+
+    public GameObject bigBear;
+    public GameObject pcCamera;
+
+    public Transform bigBearT;
+
+    public PlayerV2 player;
     public LightEventLoop2 Loop2;
+
+    public bool lookAtBear;
+
+    private void Start()
+    {
+        lookAtBear = false;
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            player.moveSpeed = 0;
-            bigBear.SetActive(true);
-            pcCamera.transform.LookAt(bigBearT);
-            lightList[0].SetActive(true);
-            lightList[1].SetActive(true);
-            lightList[2].SetActive(true);
-            lightList[3].SetActive(true);
-            lightList[4].SetActive(true);
+            SurpriseOn();
         }
+    }
 
+    IEnumerator SurpriseEventCO()
+    {
+        yield return new WaitForSeconds(2f);
+        bigBear.SetActive(true);
+        lookAtBear = true;
+        Loop2.lightList[0].SetActive(true);
+        Loop2.lightList[1].SetActive(true);
+        Loop2.lightList[2].SetActive(true);
+        Loop2.lightList[3].SetActive(true);
+        Loop2.lightList[4].SetActive(true);
+        yield return new WaitForSeconds(5f);
+        lookAtBear = false;
+
+    }
+
+    public void LookAtBear()
+    {
+        if (!lookAtBear) return;
+
+        pcCamera.transform.LookAt(bigBearT);
+    }
+
+    private void Update()
+    {
+        LookAtBear();
+    }
+
+    public void SurpriseOn()
+    {
+        StartCoroutine(SurpriseEventCO());
     }
 }
