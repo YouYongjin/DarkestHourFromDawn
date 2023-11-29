201d0cfeebd417d2f7e117c49ebb6c8848c4dcf3
YouYongjin <radio020112@gmail.com>
2023-11-29 18:18:46 +0900

    fix: MonologueUIManager 구현 진행중2
    구현은 어느정도 진행했으나, 테스트를 하지 않았습니다. 코드 보강 확률 높습니다.

--- STRUGGLING_DarkestHourFromDawn/Assets/MonologueUIManager.cs
+++ STRUGGLING_DarkestHourFromDawn/Assets/MonologueUIManager.cs
@@ -8,16 +8,42 @@ public class MonologueUIManager : MonoBehaviour
     
     public GameObject backgroundImage;
 
+    public bool UIOn = false;
+
     void Awake()
     {
         instance = this;
     }
 
-    public MonologueUIList monologueDatabase;
-
-    public void DoMonologue(GameObject Text)
+    public void MonologueUIOn()
     {
-
+        StartCoroutine(MonologueEventCO());
     }
 
+    IEnumerator MonologueEventCO()
+    {
+        yield return new WaitForSeconds(0);
+        backgroundImage.SetActive(true);
+        UIOn = true;
+        yield return new WaitForSeconds(3);
+        backgroundImage.SetActive(false);
+        UIOn = false;
+    }
+
+    public MonologueUIList monologueDatabase;
+
+    public void DoMonologue(GameObject textNum)
+    {
+        MonologueUIOn();
+        if(UIOn)
+        {
+            textNum.SetActive(true);
+        }
+    }
+
+    // 매개변수에 인수 값이 없음
+    //private void Update()
+    //{
+    //    DoMonologue();
+    //}
 }
